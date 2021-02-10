1b49665c20a857ef327337bc68fe9be61441401c
pajmd <philippejmd@gmail.com>
2021-02-10 11:13:26 +0100

    only include files

---
+++ README.md
@@ -0,0 +1,5 @@
+# fm_commons
+
+Fait Maison commons is a workaround about sharing include files.
+
+It's only purpose is to hold include files common to the FM projects.
---
+++ include/fm_commons.h
@@ -0,0 +1,14 @@
+#define SECTOR_NUM 16
+#define BLOCK_NUM 4
+#define COUNTER_SECTOR (uint8_t)3
+#define COUNTER_BLOCK (uint8_t)1
+#define COUNTER_BLOCK_VALUE 365
+#define IDS_SECTOR (uint8_t)4
+#define IDS_BLOCK (uint8_t)2
+#define READER_ID_OFFSET 0
+#define CARD_ID_OFFSET 4
+#define CARD_SECONDARY_ID_OFFSET 6
+#define READER_ID_SIZE 2
+#define MAX_READER_ID 65535
+#define CARD_ID_SIZE 1
+#define MAX_CARD_ID 255
